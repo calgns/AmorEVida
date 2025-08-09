758d45cf263286ec4339dd3b815cce3ea5bc5a74
IsabelaCaldeira <isabela.ribeiro@outlook.com>
2025-08-09 13:27:35 +0200

    fixing language of map

--- Frontend/src/Pages/Contact/index.jsx
+++ Frontend/src/Pages/Contact/index.jsx
@@ -15,7 +15,7 @@ export default function Contact() {
             CNPJ: 28.467.302/0001-93</p>
           <iframe
             title="Amor e vida localização"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3820.904798101652!2d-43.86634122524787!3d-16.731601346729565!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xab54a642027c95%3A0x9b855e093d635c!2sR.%20Melo%20Viana%2C%20401%20-%20Morrinhos%2C%20Montes%20Claros%20-%20MG%2C%2039400-427%2C%20Br%C3%A9sil!5e0!3m2!1sfr!2sfr!4v1754737607997!5m2!1sfr!2sfr"
+            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3820.904798101652!2d-43.86634122524798!3d-16.73160134672958!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xab54a642027c95%3A0x9b855e093d635c!2sR.%20Melo%20Viana%2C%20401%20-%20Morrinhos%2C%20Montes%20Claros%20-%20MG%2C%2039400-427%2C%20Brasil!5e0!3m2!1spt-BR!2sfr!4v1754738734048!5m2!1spt-BR!2sfr"
             width="654px"
             height="341px"
             style={{ border: 0 }}
